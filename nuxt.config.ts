7ad0d7eb5b5557636c9488c6a20203c47259200a
DrowKid <135056994+drowkid-1@users.noreply.github.com>
2023-08-12 14:44:47 -0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 export default ({
+    target: 'static',
+    router: { 
+        base: '/<drowlinks>/'  } ,
     modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@vueuse/nuxt'],
     build: {
         transpile: ["@headlessui/vue"],
